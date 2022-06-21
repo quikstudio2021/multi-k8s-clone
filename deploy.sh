2456a0d07975c02829d5ca54dbdcee21793d1e89
quikstudio2021 <quikstudio2021@gmail.com>
2022-06-21 21:47:24 +0100

    Cluster deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,16 @@
-docker build -t ymtangab/multi-client -f ./client/Dockerfile ./client
-docker build -t ymtangab/multi-server -f ./server/Dockerfile ./server
-docker build -t ymtangab/multi-worker -f ./worker/Dockerfile ./worker
-docker push ymtangab/multi-client
-docker push ymtangab/multi-server
-docker push ymtangab/multi-worker
+docker build -t ymtangab/multi-client:latest -t ymtangab/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t ymtangab/multi-server:latest -t ymtangab/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t ymtangab/multi-worker:latest -t ymtangab/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push ymtangab/multi-client:latest
+docker push ymtangab/multi-server:latest
+docker push ymtangab/multi-worker:latest
+
+docker push ymtangab/multi-client:$SHA
+docker push ymtangab/multi-server:$SHA
+docker push ymtangab/multi-worker:$SHA
+
+kubectl apply -f k8s/
+kubectl set image deployment/server-deployment server=ymtangab/multi-server:$SHA
+kubectl set image deployment/client-deployment server=ymtangab/multi-client:$SHA
+kubectl set image deployment/worker-deployment server=ymtangab/multi-worker:$SHA
