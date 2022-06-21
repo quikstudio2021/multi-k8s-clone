93890fd65f63e37479b7d1ba4b4d858b3224ec79
quikstudio2021 <quikstudio2021@gmail.com>
2022-06-21 21:58:51 +0100

    Simply image name

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,19 @@
-docker build -t ymtangab/multi-client:latest -t ymtangab/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t ymtangab/multi-server:latest -t ymtangab/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t ymtangab/multi-worker:latest -t ymtangab/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+export CLIENT_IMAGE="$CLIENT_IMAGE"
+export SERVER_IMAGE="$SERVER_IMAGE"
+export WORKER_IMAGE="$WORKER_IMAGE"
+docker build -t $CLIENT_IMAGE:latest -t $CLIENT_IMAGE:$SHA -f ./client/Dockerfile ./client
+docker build -t $SERVER_IMAGE:latest -t $SERVER_IMAGE:$SHA -f ./server/Dockerfile ./server
+docker build -t $WORKER_IMAGE:latest -t $WORKER_IMAGE:$SHA -f ./worker/Dockerfile ./worker
 
-docker push ymtangab/multi-client:latest
-docker push ymtangab/multi-server:latest
-docker push ymtangab/multi-worker:latest
+docker push $CLIENT_IMAGE:latest
+docker push $SERVER_IMAGE:latest
+docker push $WORKER_IMAGE:latest
 
-docker push ymtangab/multi-client:$SHA
-docker push ymtangab/multi-server:$SHA
-docker push ymtangab/multi-worker:$SHA
+docker push $CLIENT_IMAGE:$SHA
+docker push $SERVER_IMAGE:$SHA
+docker push $WORKER_IMAGE:$SHA
 
 kubectl apply -f k8s/
-kubectl set image deployment/server-deployment server=ymtangab/multi-server:$SHA
-kubectl set image deployment/client-deployment server=ymtangab/multi-client:$SHA
-kubectl set image deployment/worker-deployment server=ymtangab/multi-worker:$SHA
+kubectl set image deployment/client-deployment client=$CLIENT_IMAGE:$SHA
+kubectl set image deployment/server-deployment server=$SERVER_IMAGE:$SHA
+kubectl set image deployment/worker-deployment worker=$WORKER_IMAGE:$SHA
