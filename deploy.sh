322eba418fc6f3dbdeda018d6c22fcb5a838d4f9
quikstudio2021 <quikstudio2021@gmail.com>
2022-06-23 15:41:29 +0100

    feat: Add Docker Cache to speed up pipeline

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 export CLIENT_IMAGE="$DOCKER_USERNAME/multi-client"
 export SERVER_IMAGE="$DOCKER_USERNAME/multi-server"
 export WORKER_IMAGE="$DOCKER_USERNAME/multi-worker"
-docker build -t $CLIENT_IMAGE:latest -t $CLIENT_IMAGE:$SHA -f ./client/Dockerfile ./client
-docker build -t $SERVER_IMAGE:latest -t $SERVER_IMAGE:$SHA -f ./server/Dockerfile ./server
-docker build -t $WORKER_IMAGE:latest -t $WORKER_IMAGE:$SHA -f ./worker/Dockerfile ./worker
+docker build --cache-from $CLIENT_IMAGE:latest -t $CLIENT_IMAGE:latest -t $CLIENT_IMAGE:$SHA -f ./client/Dockerfile ./client
+docker build --cache-from $SERVER_IMAGE:latest -t $SERVER_IMAGE:latest -t $SERVER_IMAGE:$SHA -f ./server/Dockerfile ./server
+docker build --cache-from $WORKER_IMAGE:latest -t $WORKER_IMAGE:latest -t $WORKER_IMAGE:$SHA -f ./worker/Dockerfile ./worker
 
 docker push $CLIENT_IMAGE:latest
 docker push $SERVER_IMAGE:latest
